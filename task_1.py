8f4b33f102167ea3b1b71efefc587c305e389cb0
yurawdw <urawdw@gmail.com>
2025-01-21 22:27:38 +0200

    Created first version of the function "get_days_from_today"

--- task_1.py
+++ task_1.py
@@ -2,6 +2,13 @@ from datetime import datetime
 
 def get_days_from_today(date_str: str) -> int:
     '''
+    Function returns cont of days from today to DATE_STR
+
+    Input:
+    * DATE_STR - date as the string
+
+    Output:
+    * Count of the date to the DATE_STR (integer)
     '''
     pass
 
