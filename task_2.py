8e51760d911ef6c03d3994e990274aa375d39061
yurawdw <urawdw@gmail.com>
2025-01-23 01:36:09 +0200

    Fixed the “get_numbers_ticket(min_num: int, max_num: int, quantity: int)” function. Added exception checking

--- task_2.py
+++ task_2.py
@@ -12,19 +12,24 @@ def get_numbers_ticket(min_num: int, max_num: int, quantity: int) -> list:
     Output:
     * List with QUANTITY unique integers from MIN to MAX
     '''
-
     random_list = []
 
-    if (min_num < max_num) and (min_num > 0) and (max_num <= 1000) and (quantity in range(min_num, max_num)) and (quantity <= (max_num - min_num)):
-        while len(random_list) < quantity:
-            random_num = int(random.uniform(min_num, max_num))
-            if random_num in random_list:
-                continue
-            else:
-                random_list.append(random_num)
+    try:
+        min_num, max_num, quantity = int(float(min_num)), int(float(max_num)), int(float(quantity))
+    except Exception as e:
+        print(e)
+        return random_list
+    else:
+        if 0 < min_num < max_num <= 1000 and 0 < quantity <= (max_num - min_num + 1):
+            while len(random_list) < quantity:
+                random_num = random.randint(min_num, max_num)
+                if random_num in random_list:
+                    continue
+                else:
+                    random_list.append(random_num)
 
-    random_list.sort()
-    return random_list
+        random_list.sort()
+        return random_list
 
 if __name__ == "__main__":
     lottery_numbers = get_numbers_ticket(10, 49, 2)
@@ -39,5 +44,17 @@ if __name__ == "__main__":
     lottery_numbers = get_numbers_ticket(10, 4, 5)
     print("Ваші лотерейні числа:", lottery_numbers)            
 
-    lottery_numbers = get_numbers_ticket(10,20,10)
+    lottery_numbers = get_numbers_ticket(10, 20, 10)
     print("Ваші лотерейні числа:", lottery_numbers)            
+
+    lottery_numbers = get_numbers_ticket(10, 20, 4)
+    print("Ваші лотерейні числа:", lottery_numbers)   
+
+    lottery_numbers = get_numbers_ticket(10, 11, 2)
+    print("Ваші лотерейні числа:", lottery_numbers)
+    
+    lottery_numbers = get_numbers_ticket(10, "twenty", "2")
+    print("Ваші лотерейні числа:", lottery_numbers)
+
+    lottery_numbers = get_numbers_ticket(10, 20, "10.1")
+    print("Ваші лотерейні числа:", lottery_numbers)
